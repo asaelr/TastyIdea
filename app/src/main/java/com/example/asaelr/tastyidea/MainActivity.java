9b7bde8c2f3aae983b2c03632a67b52dd9589232
Nati <natigoel15@gmail.com>
2016-04-14 16:54:14 +0300

    Nati - added main activity

--- app/src/main/java/com/example/asaelr/tastyidea/MainActivity.java
+++ app/src/main/java/com/example/asaelr/tastyidea/MainActivity.java
@@ -1,36 +1,20 @@
 package com.example.asaelr.tastyidea;
 
-import android.content.res.TypedArray;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        IngCategory.initialize(this);
         setContentView(R.layout.activity_main);
-        //Log.e("TastyIdea", "" + getSupportActionBar());
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
     }
-/*
-    public void openDialog(View v) {
-        IngredientAdder adder = new IngredientAdder() {
-            @Override
-            public void addIngredient(Ingredient ing) {
-                //TypedArray ta = getResources().obtainTypedArray(id);
-                //Toast.makeText(getApplicationContext(),"Select food "+ta.getString(0),Toast.LENGTH_SHORT).show();
-                //ta.recycle();
-                Toast.makeText(getApplicationContext(),"Select food "+ing.name,Toast.LENGTH_SHORT).show();
-            }
-        };
-        CategorySelector is = new CategorySelector(adder);
-        is.show(getFragmentManager(),"ingredient selector");
-    }*/
+
+    public void handleSearchPressed(View view)
+    {
+        startActivity(new Intent(this, SearchActivity.class));
+    }
 }
